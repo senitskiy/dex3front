2503b2f047f2332a4921b7cb085faad96ca61cfa
MaxAkkerman <bagirokovanzor01@mail.ru>
2021-06-28 01:16:00 +0300

    information about deployment state on waiting popup

--- src/components/ConnectWallet/ConnectWallet.js
+++ src/components/ConnectWallet/ConnectWallet.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {useHistory} from 'react-router-dom';
 import {closeConnecting, setCurExt, setWalletIsConnected, showPopup} from '../../store/actions/app';
@@ -13,7 +13,7 @@ import {
     getRootBalanceOF,
     getAllPairsWoithoutProvider, checkClientPairExists
 } from '../../extensions/webhook/script.js';
-import { setCreator,transfer } from '../../extensions/sdk/run';
+import {setCreator, transfer, onSharding, createDEXclient} from '../../extensions/sdk/run';
 import MainBlock from '../MainBlock/MainBlock';
 import CloseBtn from '../CloseBtn/CloseBtn';
 import Loader from '../Loader/Loader';
@@ -30,27 +30,62 @@ function ConnectWallet() {
     let poolFromToken = useSelector(state => state.poolReducer.fromToken);
     let poolToToken = useSelector(state => state.poolReducer.toToken);
 
+    const [currentStatus, setcurrentStatus] = useState("Connecting wallet")
+
     useEffect(async () => {
         let pubKey = await checkPubKey(curExt._extLib.pubkey);
         let msigAd = curExt._extLib.address;
         if(!pubKey.status) {
             let balanceOF = await getRootBalanceOF()
             if(!balanceOF.balanceOf[msigAd]) {
+                setcurrentStatus("transfer - transferring funds to root")
                 let tranferToDex = await transfer(curExt._extLib.SendTransfer, Radiance.networks['2'].dexroot, 10000000000)
                 if (tranferToDex && tranferToDex.code) {
+                    console.log("tranferToDex",tranferToDex)
+                    if(tranferToDex.code===2){
+                        //TODO check codes and popup
+                        console.log("tranferToDex WITH CODE 2")
+                        dispatch(closeConnecting());
+                        dispatch(showPopup({type: 'error', message: 'Deploy wallet pls'}));
+                        dispatch(setCurExt(""));
+                    }
                     dispatch(closeConnecting());
                     dispatch(showPopup({type: 'error', message: 'Oops, something went wrong. Please try again.'}));
                     dispatch(setCurExt(""));
+                    setcurrentStatus("Connecting wallet")
                     return
                 }
             }
+            setcurrentStatus("setCreator - registering your wallet on dex")
                 let dexCLientStatus = await setCreator(curExt);
             console.log("dexCLientStatus",dexCLientStatus)
             if((dexCLientStatus && dexCLientStatus.code) || !dexCLientStatus.status){
+                setcurrentStatus("Connecting wallet")
                 dispatch(closeConnecting());
                 dispatch(showPopup({type: 'error', message: 'Oops, something went wrong. Please try again.'}));
                 dispatch(setCurExt(""));
                 return
+            }else if(dexCLientStatus.status){
+                setcurrentStatus("onSharding - looking for the best shard for you")
+                console.log("i am on sharding")
+                let onShardingStatus = await onSharding(curExt._extLib.pubkey)
+                console.log("onSharding",onShardingStatus)
+                if(onShardingStatus.status){
+                    setcurrentStatus("createDEXclient - last step deploy dex client")
+                    let createCLientStatus = await createDEXclient(curExt,onShardingStatus.data)
+                    if(!createCLientStatus.status){
+                        dispatch(closeConnecting());
+                        dispatch(showPopup({type: 'error', message: 'Oops, something went wrong. Please try again.'}));
+                        dispatch(setCurExt(""));
+                        setcurrentStatus("Connecting wallet")
+                    }
+                    console.log("createCLientStatus",createCLientStatus)
+                }else{
+                    dispatch(closeConnecting());
+                    dispatch(showPopup({type: 'error', message: 'Oops, something went wrong. Please try again.'}));
+                    dispatch(setCurExt(""));
+                    setcurrentStatus("Connecting wallet")
+                }
             }
 
 
@@ -133,7 +168,7 @@ function ConnectWallet() {
         <MainBlock
             smallTitle={true}
             normalTitle={true}
-            title={'Connecting wallet'}
+            title={currentStatus}
             button={<CloseBtn func={closeConnecting} />}
             content={
                 <div className="connect-wallet-center">
